4f240bb28cb0fe1e440933c700485b7aa4f4bd33
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2021-11-23 14:06:23 +0100

    Initial commit

---
+++ app/src/main/java/hu/eqn34f/retroquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.eqn34f.retroquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
