bd11d6a8fe546daeab144c20ac2856769bee09c3
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2021-11-23 14:18:35 +0100

    Add view binding

--- app/src/main/java/hu/eqn34f/retroquiz/MainActivity.kt
+++ app/src/main/java/hu/eqn34f/retroquiz/MainActivity.kt
@@ -2,10 +2,16 @@ package hu.eqn34f.retroquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.eqn34f.retroquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
